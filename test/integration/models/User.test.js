f128e324d3ed81dde4ca008776d920aeab4f7ce8
kctus9 <kctusx@gmail.com>
2021-02-08 15:43:54 +0100

    fix commit User.test.js

--- test/integration/models/User.test.js
+++ test/integration/models/User.test.js
@@ -1,20 +1,63 @@
+const {v4} = require('uuid');
 const assert = require('assert');
 
+const newUserId = v4();
+const userNewId= v4();
+
 describe('User#Model', () => {
+  describe('POST', () => {
+    it('Should create Users and check ', async () => {
+      var createdUser = await User.create({
+        id: newUserId,
+        login: 'test',
+        password: 'test',
+        roleId: Role.findOne({ value: 'Admin'}).id
+      }).fetch();
+      assert(createdUser);
+
+    });
+  });
+
   describe('GET', () => {
-    it('Should get Users and check attributes', async () => {
+    it('Should get User and check attributes', async () => {
       const users = await User.find().limit(1);
       assert(users);
-      if (users.length > 0) {
-        const user = users[0];
-        assert(user);
-        assert(user.id !== null);
-        assert(user.login !== null);
-        assert(user.password !== null);
-        assert(user.roleId !== null);
-        assert(user.createdAt !== undefined);
-        assert(user.updatedAt !== undefined);
-      }
+      const user = users[0];
+      assert(user);
+      assert(user.id !== null);
+      assert(user.login !== null);
+      assert(user.password !== null);
+      assert(user.roleId !== null);
+      assert(user.createdAt !== null);
+      assert(user.updatedAt !== null);
+    });
+  });
+
+  describe('PUT', () => {
+    it('Should update User and check', async () => {
+      const idRoleAdmin = (await Role.find({ value: 'Admin'}))[0].id;
+      const updatedUser = await User.updateOne({id: newUserId})
+        .set({
+          id: userNewId,
+          login: 'TESTE',
+          password: 'TESTE',
+          roleId: idRoleAdmin
+        });
+
+      assert(updatedUser.id === userNewId);
+      assert(updatedUser.login === 'TESTE');
+      assert(updatedUser.password === 'TESTE');
+      assert(updatedUser.roleId === idRoleAdmin);
+      assert(updatedUser.createdAt !== null);
+      assert(updatedUser.updatedAt !== null);
+    });
+  });
+
+  describe('DELETE', () => {
+    it( 'Shoul delete a user and check', async () => {
+      const deletedUser = await User.destroy({id: userNewId}).fetch();
+
+      assert(deletedUser[0].id === userNewId);
     });
   });
 });
